1d4fc498108adaf42278915318a61a61e02556e9
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-07-30 14:19:26 +0200

    feat: add configuration spring mvc

---
+++ src/test/java/org/lessons/java/demo/controller/hello_spring_mvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.demo.controller.hello_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
